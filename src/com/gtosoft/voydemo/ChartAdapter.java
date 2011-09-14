bed677c985604f29d50548a759c9f1e60fd2df7a
Brad Hein <gtosoft@gmail.com>
2011-09-13 22:18:16 -0400

    Added a listview that populates with charts as new DPNs arrive.

---
+++ src/com/gtosoft/voydemo/ChartAdapter.java
@@ -0,0 +1,56 @@
+package com.gtosoft.voydemo;
+
+import java.util.List;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+
+import com.artfulbits.aiCharts.ChartView;
+
+public class ChartAdapter extends ArrayAdapter<ChartView> {
+	int resource;
+
+	public ChartAdapter(Context context, int textViewResourceId, List objects) {
+		super(context, textViewResourceId, objects);
+		
+		resource = textViewResourceId;
+	}
+
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+//		super.getView(position, convertView, parent);
+		
+//		ChartView cv = getItem(position);
+		LinearLayout chartviewlayout;
+		
+		if (convertView == null) {
+			// instantiate a new layout for the list
+			msg ("getting new linearlayout");
+			chartviewlayout = new LinearLayout(getContext());
+			String inflater = Context.LAYOUT_INFLATER_SERVICE;
+			LayoutInflater vi = (LayoutInflater) getContext().getSystemService(inflater);
+			msg ("inflating...");
+			vi.inflate(resource, chartviewlayout, true);
+			
+		} else{
+//			msg ("re-using a view");
+			// re-use existing list item. 
+			chartviewlayout = (LinearLayout) convertView;
+		}
+		
+		return chartviewlayout;
+		
+	}// end of getview()
+	
+	
+	private void msg (String m) {
+		Log.d("ChartAdapter",m);
+	}
+
+}// end of class.
--- src/com/gtosoft/voydemo/MainActivity.java
+++ src/com/gtosoft/voydemo/MainActivity.java
@@ -1,12 +1,21 @@
 package com.gtosoft.voydemo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.artfulbits.aiCharts.ChartView;
+import com.artfulbits.aiCharts.Base.ChartAxis.LabelsMode;
+import com.artfulbits.aiCharts.Base.ChartPoint;
+import com.artfulbits.aiCharts.Base.ChartPointCollection;
+import com.artfulbits.aiCharts.Base.ChartSeries;
 import com.gtosoft.libvoyager.android.ActivityHelper;
 import com.gtosoft.libvoyager.db.DashDB;
 import com.gtosoft.libvoyager.session.HybridSession;
@@ -15,8 +24,6 @@ import com.gtosoft.libvoyager.util.EasyTime;
 import com.gtosoft.libvoyager.util.EventCallback;
 import com.gtosoft.libvoyager.util.GeneralStats;
 import com.gtosoft.libvoyager.util.OOBMessageTypes;
-// Thanks Lincoln :)
-//import static com.gtosoft.libvoyager.util.OOBMessageTypes.*;
 
 
 public class MainActivity extends Activity {
@@ -43,6 +50,10 @@ public class MainActivity extends Activity {
 	
 	String mBTPeerAddr = "";
 	
+	ChartAdapter chartAdapter;
+	
+	List<ChartView> chartArray = new ArrayList<ChartView>();
+	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,9 +64,132 @@ public class MainActivity extends Activity {
         aHelper = new ActivityHelper(this);
         // Register with libvoyager to receive "we found an ELM device nearby" message so when we perform a discovery, this method gets called.
         aHelper.registerChosenDeviceCallback(chosenCallback);
-        // in the onResume method we will make the call to ActivityHelper to actually kick off the bluetooth discovery. 
+        // in the onResume method we will make the call to ActivityHelper to actually kick off the bluetooth discovery.
+        
+        chartAdapter = new ChartAdapter(this, R.layout.chartlistitem, chartArray);
+        
+        ListView lv = (ListView) findViewById(R.id.lvCharts);
+        lv.setAdapter(chartAdapter);
+    }
+    
+    private boolean doesChartExist (String title) {
+    	for (ChartView c : chartArray) {
+			if (c.getTitles().get(0).getText().equals(title)) {
+				return true;
+			}
+		}
+    	
+    	return false;
     }
 
+    private ChartView addChart (String title) {
+    	msg ("Adding chart with title " + title);
+    	ChartView c = new ChartView(this,R.xml.shortchart);
+    	
+    	setXYTitles(c, "X", "Y", title);
+    	clearAllGraphPoints(c);
+    	
+        chartArray.add(c);
+        
+        return c;
+    }
+
+    /**
+     * Looks for chart with given title and returns it. Otherwise returns null. 
+     * @param title
+     * @return
+     */
+    private ChartView getChartByTitle (String title) {
+    	for (ChartView c : chartArray) {
+			if (c.getTitles().get(0).getText().equals(title)) {
+				return c;
+			}
+		}
+
+    	// otherwise, create it!
+    	return addChart(title);
+    	
+    }
+    
+    private void setXYTitles (final ChartView cv, final String newXTitle, final String newYTitle, final String newGraphTitle) {
+        muiHandler.post(new Runnable () {
+                public void run () {
+                        cv.getSeries().get(0).getActualXAxis().setTitle(newXTitle);
+                        cv.getSeries().get(0).getActualYAxis().setTitle(newYTitle);
+                        cv.getSeries().get(0).getActualYAxis().setLabelsMode(LabelsMode.RangeLabels);
+                        cv.getSeries().get(0).getActualYAxis().setGridVisible(true);
+                        cv.getSeries().get(0).getActualYAxis().setShowLabels(true);
+                        cv.getTitles().get(0).setText(newGraphTitle);
+                }// end of run
+        });// end of post
+    }// end of setXYTitles
+    
+    private void addPointToGraph (final ChartView cv, final double X, final double Y) {
+        final ChartPoint point = new ChartPoint(X, Y);
+        
+
+        // post it to the UI thread.
+        muiHandler.post(new Runnable () {
+                public void run () {
+                        //cv.getSeries().get(0).getArea().
+                        cv.getSeries().get(0).getPoints().add(point);
+                        
+                        // scroll if necessary.
+                        int numPoints = cv.getSeries().get(0).getPoints().size();
+                        // remove the leftmost point. This has the affect of scrolling...
+                        if (numPoints > 100) {
+                                cv.getSeries().get(0).getPoints().removeAt(0);
+                                cv.getSeries().get(0).getActualXAxis().getScale().setMinimum(X - 100);
+                                cv.getSeries().get(0).getActualXAxis().getScale().setMaximum(X);
+                        }// end of scroll check. 
+                }// end of run
+        }); // end of post
+    }// end of addPointToGraph...
+    
+    
+
+    private void addPointToGraphSimple(final String title, final double Y) {
+    	
+    	if (doesChartExist(title) == false) {
+    		muiHandler.post(new Runnable () {
+    			public void run () {
+    				addChart(title);
+    				msg ("Added chart " + title + " in background.");
+    			}
+    		});
+    		EasyTime.safeSleep(5000);
+    	}
+
+    	ChartView cv = getChartByTitle(title);
+    	
+    	if (cv == null) {
+    		msg ("Chart not found! title=" + title);
+    		return;
+    	}
+    	
+    	double X = 0;
+
+    	// see if there's already an X point, if so, add one to it. 
+    	try {
+    		int numpoints = cv.getSeries().get(0).getPoints().size();
+			X = cv.getSeries().get(0).getPoints().get(numpoints-1).getX() + 1;
+		} catch (Exception e) {
+			msg ("Error getting last X for chart " + title);
+			// in this case we fall back on default X. 
+		}
+        msg ("Adding point (" + X + "," + Y + ") to chart " + title);
+    	addPointToGraph(cv, X, Y);
+    	
+    	muiHandler.post(new Runnable () {
+    		public void run () {
+    			chartAdapter.notifyDataSetChanged();
+    		}
+    	});
+    }
+
+    
+    
+    
     @Override
     protected void onResume() {
     	super.onResume();
@@ -65,9 +199,27 @@ public class MainActivity extends Activity {
         //    If the device seems to be an ELM OBD device, it calls the "chosenCallback" event with details about the device (its MAC). 
         if (hs == null || hs.getEBT().isConnected() != true) {
         	msg ("Performing bluetooth discovery...");
-        	aHelper.startDiscovering();
+        	connectToBestAvailable();
         } else {
         	msg ("Unable to perform bluetooth discovery. BT Connected = " + hs.getEBT().isConnected());
+        }
+        
+        
+    }
+
+    /**
+     * Either connects to last known device, or performs a discovery to find nearby OBD devices. 
+     */
+    private void connectToBestAvailable() {
+        String lastmac = aHelper.getLastUsedMAC();
+        
+        // Connect to last known device if possible. Otherwise do the usual bluetooth scan.  
+        if (lastmac.length() == 17) {
+        	if (DEBUG) msg ("Using last known device MAC!");
+        	setupSession(lastmac);
+        } else {
+	        if (DEBUG) msg ("No preferred device has been selected. Scanning...");
+	        aHelper.startDiscovering();
         }
     }
     
@@ -122,6 +274,8 @@ public class MainActivity extends Activity {
 
   	 
   	  msg ("Setting up hybridSession. It will now establish a bluetooth connection.");
+  	  
+  	  aHelper.setLastUsedMAC(deviceMACAddress);
   	  
   	  // instantiate hybridsession, which is just a class that controls subclasses such as Monitorsession and OBDSession, that communicate with the network in different ways.
   	  hs = new HybridSession (BluetoothAdapter.getDefaultAdapter(), deviceMACAddress, ddb, ecbOOBMessageHandler);
@@ -203,6 +357,8 @@ public class MainActivity extends Activity {
 		public void onDPArrived(String DPN, String sDecodedData, int iDecodedData) {
 			
 			msg ("(DP Arrived) " + DPN + "=" + sDecodedData);
+			
+			addPointToGraphSimple(DPN, getPrimaryDPNValue(sDecodedData));
 			
 			//			if (DPN.equals("SPEED")) {
 			//				addPointToSpeedGraph(sDecodedData);
@@ -288,4 +444,42 @@ public class MainActivity extends Activity {
 		showMsg(m);
 		Log.d("Activity",m);
 	}
+
+	/**
+	 * removes commas and takes just the first value, if present. 
+	 * @param decodedData
+	 * @return
+	 */
+	Double getPrimaryDPNValue (String decodedData) {
+		Double Y = 0d;
+		
+		// do our best to extract the data value. 
+		if (decodedData.contains(",")) {
+			try {Y = Double.valueOf(decodedData.split(",")[0]);
+			} catch (NumberFormatException e) { }
+		} else {
+			try {Y = Double.valueOf(decodedData);
+			} catch (NumberFormatException e) { }
+		}
+
+		return Y;
+	}
+	
+    private void clearAllGraphPoints (ChartView cv) {
+        
+        try {
+                ChartSeries cs = cv.getSeries().get(0);
+                ChartPointCollection points = cs.getPoints();
+                points.clear();
+                
+        } catch (Exception e) {
+                if (DEBUG) msg ("Error clearing chart points. E=" + e.getMessage());
+        }
+}
+
+	
+	
+	
+	
+	
 }
